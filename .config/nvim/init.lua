6de78e5d275908d8d14f82d64d6bacde58847aa3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-04 17:20:33 +0530

    Enable lualine icons & Remove component separators

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,9 +182,8 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
         theme = 'onedark',
-        component_separators = '~',
+        component_separators = '',
         section_separators = '',
       },
     },
