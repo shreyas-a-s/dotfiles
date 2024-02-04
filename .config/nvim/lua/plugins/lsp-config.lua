943246bd3776961618c114d3682d099ab776e146
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-04 23:23:33 +0530

    Remove html from lsp-config.lua

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -20,9 +20,6 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
