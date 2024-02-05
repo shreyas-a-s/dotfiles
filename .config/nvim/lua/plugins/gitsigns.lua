109ebb670e786ef80ab3f423b9a22185aeca091e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-06 00:14:41 +0530

    Reduce gitsigns line_blame delay from 1000 -> 250

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,11 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      current_line_blame_opts = { delay = 250 }
+    })
 
     vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
     vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-  end
+  end,
 }
