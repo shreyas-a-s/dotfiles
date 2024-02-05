9d005092517db53597ae8c2a25cab9ccef91c0cc
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-05 18:59:01 +0530

    Remap keybindings for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,8 +15,8 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>.", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>rg", builtin.live_grep, {})
 
       require("telescope").load_extension("ui-select")
     end,
