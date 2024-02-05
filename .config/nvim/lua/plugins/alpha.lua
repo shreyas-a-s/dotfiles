149740e8363ada06c412aa37ceee4cd8e3111795
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-05 19:22:00 +0530

    Formatting lua scripts using stylua

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,16 +1,16 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.startify")
 
-    -- Disable header
-    dashboard.section.header.val = {}
+		-- Disable header
+		dashboard.section.header.val = {}
 
-    alpha.setup(dashboard.opts)
-  end,
+		alpha.setup(dashboard.opts)
+	end,
 }
