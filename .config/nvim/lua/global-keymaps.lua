b2925dd41adda15a802d9f23ddaf92c2eac910e2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-05 18:20:14 +0530

    Separate vim-options & global-keymaps

---
+++ .config/nvim/lua/global-keymaps.lua
@@ -0,0 +1,12 @@
+-- Setting the leader key to a space character
+vim.g.mapleader = " "
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Set keymap to hide search highlight
+vim.keymap.set('n', '<leader>nh', ':nohlsearch<CR>')
+
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,16 +1,9 @@
--- General options
+-- Use 2 spaces instead of tabs for indentation
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- Enable line numbers
 vim.wo.number = true
 
