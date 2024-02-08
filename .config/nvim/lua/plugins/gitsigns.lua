bde8584ee967c8830cb305b6e3417d8673604141
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-08 18:06:28 +0530

    Update gitsigns with better signs

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,13 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup({
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
       current_line_blame_opts = { delay = 250 }
     })
   end,
