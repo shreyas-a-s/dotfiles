3734866c6b9507a4b7540de8f0f78bad943f26ff
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-08 18:47:21 +0530

    Format colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     name = "kanagawa",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "kanagawa"
-    end
-  }
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
 }
