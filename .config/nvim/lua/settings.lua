2ae1c70ae6810d0b1871996af9663262896aa393
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-09 14:16:50 +0530

    Simplify settings.lua

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,37 +1,40 @@
+-- Local variable to simplify code
+local opt = vim.opt
+
 -- Use 2 spaces instead of tabs for indentation
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 
 -- Enable 24-bit colors
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
 -- Enable line numbers
-vim.wo.number = true
+opt.number = true
 
 -- Enable relative line numbers
-vim.wo.relativenumber = true
+opt.relativenumber = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
+opt.signcolumn = "yes"
 
 -- Enable break indent
-vim.opt.breakindent = true
+opt.breakindent = true
 
 -- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
+opt.updatetime = 250
+opt.timeoutlen = 300
 
 -- Sync clipboard between OS and Neovim.
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
