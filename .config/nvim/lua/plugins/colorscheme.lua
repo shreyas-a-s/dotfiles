9664927de4317ad309828a818c95ac5a210e9f02
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-09 20:41:10 +0530

    Minor indentation changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    name = "kanagawa",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("kanagawa")
-    end,
-  },
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  name = "kanagawa",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("kanagawa")
+  end,
 }
