25eb0b602ec360054cc14051457941b533b2fa9b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-12 00:53:18 +0530

    Format which-key.lua

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -6,14 +6,14 @@ return {
     vim.o.timeoutlen = 300
   end,
   opts = {},
-  config = function ()
-  local wk = require("which-key")
-  wk.register({
-    b = { name = "buffers" },
-    c = { name = "code" },
-    g = { name = "git" },
-    f = { name = "file" },
-    w = { name = "window" },
-  }, { prefix = "<leader>" })
+  config = function()
+    local wk = require("which-key")
+    wk.register({
+      b = { name = "buffers" },
+      c = { name = "code" },
+      g = { name = "git" },
+      f = { name = "file" },
+      w = { name = "window" },
+    }, { prefix = "<leader>" })
   end,
 }
