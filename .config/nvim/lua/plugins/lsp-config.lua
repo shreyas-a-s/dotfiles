83a03656e72bac1c5716dd4f8354252d4a6108b3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-12 00:07:04 +0530

    Add html and eslint to lsp-config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -17,11 +17,11 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
     end,
   },
 }
