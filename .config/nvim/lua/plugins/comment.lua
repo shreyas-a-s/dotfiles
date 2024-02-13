7c32d8c6c669a85f281545d2ee4c3a6b9dd9f274
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-13 19:19:34 +0530

    Minor tweaks

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -10,7 +10,7 @@ return {
       enable_autocmd = false,
     })
     require("Comment").setup({
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     })
   end,
 }
