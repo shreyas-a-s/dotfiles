56d401ac6ad83756816239c0af33a3a1394074c9
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-14 21:04:25 +0530

    Disable nvim-colorizer in Lazy

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,11 @@
 return {
-	"NvChad/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup()
-	end,
+  "NvChad/nvim-colorizer.lua",
+  config = function()
+    require("colorizer").setup({
+      filetypes = {
+        "*",
+        "!lazy",
+      },
+    })
+  end,
 }
