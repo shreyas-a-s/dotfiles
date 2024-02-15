7d666a7bab2cb7dfd63b05e30b4f7185430941e7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-16 01:52:14 +0530

    Change colorscheme from kanagawa -> tokyonight

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = false,
-        theme = "onedark",
+        theme = "auto",
         component_separators = "│",
         section_separators = "",
         disabled_filetypes = { "alpha" },
