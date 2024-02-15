e9bad40aa62dcb45ff0388f09cb30cb66c6f9b46
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-15 23:40:03 +0530

    Simplify which-key.lua

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,14 +5,5 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      b = { name = "buffers" },
-      c = { name = "code" },
-      g = { name = "git" },
-      f = { name = "file" },
-      w = { name = "window" },
-    }, { prefix = "<leader>" })
-  end,
+  opts = {},
 }
