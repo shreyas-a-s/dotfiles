1f1d88e99a82b55afe4b3fc2c1dad3802b450317
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-18 01:29:46 +0530

    Change nvim-notify style

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -2,7 +2,6 @@ return {
   "rcarriga/nvim-notify",
   config = function ()
     require("notify").setup({
-      render = "minimal",
       timeout = 500,
     })
     vim.notify = require("notify")
