316a39008436a44c6034fc492197cf23612b934a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-18 02:13:58 +0530

    Add beautysh to format shell scripts & Organise code

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,14 +5,26 @@ return {
   },
   config = function()
     local null_ls = require("null-ls")
+    local diagnostics = null_ls.builtins.diagnostics
+    local formatting = null_ls.builtins.formatting
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
+        -- Lua
+        formatting.stylua,
+
+        -- Shell scripts
+        diagnostics.shellcheck,
+        formatting.beautysh.with({
+          extra_args = { "-i", "2" },
+        }),
+
+        -- Python
+        formatting.black,
+        formatting.isort,
+
+        -- Web dev (html, js, css. etc.)
+        diagnostics.eslint,
+        formatting.prettier,
       },
     })
   end,
