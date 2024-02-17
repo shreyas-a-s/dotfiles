470e1fcc4a0059cdf7cd09b54c8dba170b5d7ed8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-18 00:42:18 +0530

    Add telescope-fzf-native to speed up fuzzyfinding

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
   },
   config = function()
     require("telescope").setup({
@@ -14,5 +18,6 @@ return {
       },
     })
     require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("fzf")
   end,
 }
