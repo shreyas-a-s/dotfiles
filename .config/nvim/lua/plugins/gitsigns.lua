a081a6d13d71f0214d261dfcf319c1bf2e467d99
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-20 23:44:06 +0530

    Simplify gitsigns.lua

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      signs = {
-        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      },
-      current_line_blame_opts = { delay = 250 }
-    })
-  end,
+  opts = {
+    signs = {
+      add          = { text = "▎" },
+      change       = { text = "▎" },
+      delete       = { text = "▎" },
+      topdelete    = { text = "▎" },
+      changedelete = { text = "▎" },
+    },
+    current_line_blame_opts = { delay = 250 }
+  },
 }
