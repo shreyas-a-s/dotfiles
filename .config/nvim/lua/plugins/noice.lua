7004f3ba05f3707fb492812c01d73aefadadf9dd
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-22 00:32:41 +0530

    Move away from nvim-notify to noice.nvim

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,36 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    {
+      "rcarriga/nvim-notify",
+      config = function ()
+        require("notify").setup({
+          timeout = 750,
+        })
+      end,
+    },
+  },
+  opts = {
+    cmdline = {
+      view = "cmdline",
+      format = {
+        cmdline = false,
+        search_down = false,
+        search_up = false,
+      },
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+  },
+}
--- .config/nvim/lua/plugins/nvim-notify.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  config = function ()
-    require("notify").setup({
-      timeout = 500,
-    })
-    vim.notify = require("notify")
-  end,
-}
