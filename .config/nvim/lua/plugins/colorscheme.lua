1886f8d995ab9617c97255c590321517f07fa50c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-22 23:49:39 +0530

    Change colorscheme from kanagawa -> solarized-osaka

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "rebelot/kanagawa.nvim",
+  "craftzdog/solarized-osaka.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("kanagawa")
+    vim.cmd.colorscheme("solarized-osaka")
   end,
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = false,
-        theme = "onedark",
+        theme = "auto",
         component_separators = "┊",
         section_separators = "",
         disabled_filetypes = { "alpha" },
