36197a7d1c4a5fc7e9cdde1a974d673dc87a00f5
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-23 00:35:09 +0530

    Add filetypes & buftypes to exclude in ncim-colorizer.lua

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,24 @@
 return {
   "NvChad/nvim-colorizer.lua",
   opts = {
-    filetypes = { "*", "!lazy" },
+    filetypes = {
+      "*",
+      "!alpha",
+      "!dashboard",
+      "!neo-tree",
+      "!NvimTree",
+      "!Trouble",
+      "!trouble",
+      "!lazy",
+      "!mason",
+      "!notify",
+      "!toggleterm",
+      "!lazyterm",
+    },
+    buftypes = {
+      "*",
+      "!prompt",
+      "!popup"
+    },
   },
 }
