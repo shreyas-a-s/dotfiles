dfbd68c30423b068aa42aff6c1c3811c6ac17db4
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-22 14:20:23 +0530

    Add mini.indentscope

--- .config/nvim/lua/plugins/indents.lua
+++ .config/nvim/lua/plugins/indents.lua
@@ -1,8 +1,17 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-    scope = { enabled = false },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = "┊" },
+      scope = { enabled = false },
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    version = "*",
+    opts = {
+      symbol = "┊"
+    },
   },
 }
