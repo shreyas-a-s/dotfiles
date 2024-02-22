9b0563368fcd9746973a6c75e1497d92c5c52c56
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-22 13:29:15 +0530

    Add keymaps for j,k to deal with word wrap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -64,3 +64,7 @@ map("v", ">", ">gv")
 
 -- Disable buffer content change when pasting in Visual mode
 map("v", "p", "P", { desc = "Paste in visual mode" })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
