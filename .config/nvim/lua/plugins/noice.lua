9dd3622fd56ac15822ef08a1469dcd26b7c8058e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-22 14:43:24 +0530

    Simplify noice.lua and disable lsp progress

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -5,7 +5,7 @@ return {
     "MunifTanjim/nui.nvim",
     {
       "rcarriga/nvim-notify",
-      config = function ()
+      config = function()
         require("notify").setup({
           timeout = 750,
         })
@@ -24,10 +24,8 @@ return {
       },
     },
     lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+      progress = {
+        enabled = false,
       },
     },
     presets = {
