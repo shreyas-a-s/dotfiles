be02c03acef01f0243dbd9fdeb6f6ec2d843a12d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-24 00:50:57 +0530

    Add keymaps to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -30,6 +30,14 @@ return {
         },
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-Space>",
+            node_incremental = "<C-Space>",
+            scope_incremental = "<C-s>",
+          },
+        },
       })
     end,
   },
