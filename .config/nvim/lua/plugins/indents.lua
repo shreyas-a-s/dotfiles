45245a3f466ad886286b65f31b804f321fed3fc6
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-23 19:04:39 +0530

    Remove mini.indentscope

--- .config/nvim/lua/plugins/indents.lua
+++ .config/nvim/lua/plugins/indents.lua
@@ -1,38 +1,8 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-      indent = { char = "┊" },
-      scope = { enabled = false },
-    },
-  },
-  {
-    "echasnovski/mini.indentscope",
-    version = "*",
-    opts = {
-      symbol = "┊",
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "NvimTree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = { char = "┊" },
+    scope = { enabled = false },
   },
 }
