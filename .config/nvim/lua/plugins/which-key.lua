3391fab9d6bc81b6db98973e7ceb4404abe6b85f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-24 14:03:11 +0530

    Make which-key window simpler

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,4 +1,18 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  opts = {
+    plugins = {
+      presets = {
+        z = false,
+        g = false,
+      },
+    },
+    window = {
+      border = "rounded",
+      padding = { 1, 1, 1, 1 },
+    },
+    show_help = false,
+    show_keys = false,
+  },
 }
