4ec1715186b29bba7248b5f48f3ef6c1120b0eab
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-26 18:51:15 +0530

    Add mason-tool-installer to complement none-ls

---
+++ .config/nvim/lua/user/extras/masontoolinstaller.lua
@@ -0,0 +1,19 @@
+local M = {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = { "williamboman/mason.nvim" },
+}
+
+function M.config()
+  require("mason-tool-installer").setup({
+    ensure_installed = {
+      "stylua",
+      "prettier",
+      "black",
+      "isort",
+      "shfmt",
+    },
+  })
+end
+
+return M
+
