322ba96bfbc789a51af7701c69c815823c11634d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-02 00:56:01 +0530

    Remove 'diagnostics on CursorHold'

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -17,13 +17,6 @@ end
 
 M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
-
-  -- Show diagnostics popup
-  vim.api.nvim_create_autocmd({ "CursorHold" }, {
-    callback = function()
-      vim.diagnostic.open_float(nil,{focusable=false})
-    end,
-  })
 end
 
 function M.common_capabilities()
