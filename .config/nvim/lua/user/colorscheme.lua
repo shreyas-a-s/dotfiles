42f5630400c2bffd6d6cef007f03103fb7b587d8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-06 23:42:50 +0530

    Revert to tiagovla/tokyodark.nvim from mine coz it was fine this whole time

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,10 +1,18 @@
 local M = {
-  "shreyas-a-s/tokyodark.nvim",
+  "tiagovla/tokyodark.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
-
 function M.config()
+  require("tokyodark").setup({
+    styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+        functions = {},
+        variables = {},
+    },
+  })
   vim.cmd.colorscheme("tokyodark")
 end
 
