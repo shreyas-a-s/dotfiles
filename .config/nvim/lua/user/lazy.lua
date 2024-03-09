d002dcd1d2a43a1fb117a20afdfaea4ed525b1fb
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-09 13:41:45 +0530

    Disable setting colorscheme to lazy.nvim

--- .config/nvim/lua/user/lazy.lua
+++ .config/nvim/lua/user/lazy.lua
@@ -13,7 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = LAZY_PLUGIN_SPEC,
-  install = { colorscheme = { "darkplus", "default" } },
   ui = { border = "rounded" },
   change_detection = { notify = false },
 })
