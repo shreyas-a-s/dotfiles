a64f05aa576831d04395d482e414dcd8588f2baf
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-12 00:27:07 +0530

    Change colorscheme from tokyodark -> my own neodarker

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,30 +1,10 @@
 local M = {
-  "tiagovla/tokyodark.nvim",
+  "shreyas-a-s/neodarker.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 function M.config()
-  require("tokyodark").setup({
-    styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        identifiers = { italic = false },
-        functions = {},
-        variables = {},
-    },
-  })
-  vim.cmd.colorscheme("tokyodark")
-
-  -- Set right color to lualine section_z
-  require("lualine").setup({
-    options = {
-      theme = function()
-        local tokyodark = require("lualine.themes.tokyodark")
-        tokyodark.normal.z = tokyodark.normal.a
-        return tokyodark
-      end,
-    },
-  })
+  vim.cmd.colorscheme("neodarker")
 
   -- Change some colors
   vim.cmd([[
