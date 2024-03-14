f58e1d707ceaa8a3f9ed367f607498a8fb8aa05c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-14 16:01:22 +0530

    feat: Set conceallevel to 3 for markdown files

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -65,6 +65,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+    vim.opt_local.conceallevel = 3
   end,
 })
 
