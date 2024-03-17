d54d4bed1d5a61b67291c4f570f509f84590f863
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-17 18:39:47 +0530

    fix(nvim-tree): Remove its dependence on icons.lua

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -9,21 +9,19 @@ function M.config()
     ["<leader><space>"] = { "<CMD>NvimTreeToggle<CR>", "Explorer" },
   })
 
-  local icons = require("user.icons")
-
   require("nvim-tree").setup({
     hijack_netrw = false,
     renderer = {
       icons = {
         glyphs = {
           git = {
-            unstaged = icons.git.FileUnstaged,
-            staged = icons.git.FileStaged,
-            unmerged = icons.git.FileUnmerged,
-            renamed = icons.git.FileRenamed,
-            untracked = icons.git.FileUntracked,
-            deleted = icons.git.FileDeleted,
-            ignored = icons.git.FileIgnored,
+            unstaged = "",
+            staged = "S",
+            unmerged = "",
+            renamed = " ",
+            untracked = "U",
+            deleted = " ",
+            ignored = "◌",
           },
         },
       },
