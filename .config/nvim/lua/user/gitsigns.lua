986d6bdf1a673d115706c2de9a3acf689cab892a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-04-06 23:31:59 +0530

    refactor(gitsigns.nvim): Update keymaps
    - Remove the ones I didn't use
    - Change toggle line blame keymap from <SPC>gt to <SPC>gb

--- .config/nvim/lua/user/gitsigns.lua
+++ .config/nvim/lua/user/gitsigns.lua
@@ -6,15 +6,12 @@ local M = {
 M.config = function()
   local wk = require("which-key")
   wk.register({
-    ["<leader>gj"] = { "<CMD>lua require 'gitsigns'.next_hunk({navigation_message = false})<CR>", "Next Hunk" },
-    ["<leader>gk"] = { "<CMD>lua require 'gitsigns'.prev_hunk({navigation_message = false})<CR>", "Prev Hunk" },
     ["<leader>gp"] = { "<CMD>lua require 'gitsigns'.preview_hunk()<CR>", "Preview Hunk" },
     ["<leader>gr"] = { "<CMD>lua require 'gitsigns'.reset_hunk()<CR>", "Reset Hunk" },
-    ["<leader>gl"] = { "<CMD>lua require 'gitsigns'.blame_line()<CR>", "Line Blame" },
     ["<leader>gR"] = { "<CMD>lua require 'gitsigns'.reset_buffer()<CR>", "Reset Buffer" },
     ["<leader>gs"] = { "<CMD>lua require 'gitsigns'.stage_hunk()<CR>", "Stage Hunk" },
     ["<leader>gu"] = { "<CMD>lua require 'gitsigns'.undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-    ["<leader>gt"] = { "<CMD>lua require 'gitsigns'.toggle_current_line_blame()<CR>", "Toggle line blame" },
+    ["<leader>gb"] = { "<CMD>lua require 'gitsigns'.toggle_current_line_blame()<CR>", "Toggle line blame" },
   })
 
   require("gitsigns").setup({
