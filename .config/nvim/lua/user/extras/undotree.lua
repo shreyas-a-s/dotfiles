61a0445aa8bb1bd033611aff8bb4198fad9d6fd7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-01-28 00:43:02 +0530

    Make undotree in neovim better

--- .config/nvim/lua/user/extras/undotree.lua
+++ .config/nvim/lua/user/extras/undotree.lua
@@ -3,6 +3,12 @@ local M = {
 }
 
 function M.config()
+	-- Configs
+	vim.g.undotree_SetFocusWhenToggle = true
+	vim.g.undotree_DiffAutoOpen = false
+	vim.g.undotree_ShortIndicators = true
+
+	-- Keymaps
 	vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "Undotree" })
 end
 
