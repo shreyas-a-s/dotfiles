d6e8e3ca4e49f8494c879cb0dc4a5c4ae625cf3e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-02-13 00:24:44 +0530

    Format project.lua

--- .config/nvim/lua/user/project.lua
+++ .config/nvim/lua/user/project.lua
@@ -1,12 +1,12 @@
 local M = {
-  "ahmedkhalf/project.nvim",
-  event = "VeryLazy",
+	"ahmedkhalf/project.nvim",
+	event = "VeryLazy",
 }
 
 function M.config()
-  require("project_nvim").setup({
-    detection_methods = { "pattern" },
-  })
+	require("project_nvim").setup({
+		detection_methods = { "pattern" },
+	})
 end
 
 return M
