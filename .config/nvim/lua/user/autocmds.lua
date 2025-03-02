026da2480fd501db3ba40d8287b00288b1bc438a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-03-02 21:43:26 +0530

    Format autocmds.lua

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -1,17 +1,17 @@
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Enable line wrap and conceal in these filetypes
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.conceallevel = 3
-  end,
+	pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.conceallevel = 3
+	end,
 })
